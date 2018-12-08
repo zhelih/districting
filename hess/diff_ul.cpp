9835248f4c539454d97cd599b66f42725231805a
Eugene Lykhovyd <lykhovyd@gmail.com>
2018-12-07 18:53:52 -0600

    add U-L second formulation

--- hess/diff_ul.cpp
+++ hess/diff_ul.cpp
@@ -27,7 +27,7 @@ GRBVar** build_UL_1(GRBModel* model, graph* g, const vector<int>& population, in
 
   model->setObjective(lu[1] - lu[0], GRB_MINIMIZE);
 
-  // add constraints (37b)
+  // add constraints (36b)
   for(int i = 0; i < n; ++i)
   {
     GRBLinExpr constr = 0;
@@ -36,13 +36,13 @@ GRBVar** build_UL_1(GRBModel* model, graph* g, const vector<int>& population, in
     model->addConstr(constr == 1);
   }
 
-  // add constraint (37c)
+  // add constraint (36c)
   GRBLinExpr expr = 0;
   for(int j = 0; j < n; ++j)
     expr += x[j][j];
   model->addConstr(expr == k);
 
-  // add constraint (37d)
+  // add constraint (36d)
   for(int j = 0; j < n; ++j)
   {
     GRBLinExpr constr = 0;
@@ -53,18 +53,66 @@ GRBVar** build_UL_1(GRBModel* model, graph* g, const vector<int>& population, in
     model->addConstr(constr - lu[0] - p_bar * x[j][j] >= -p_bar); // L
   }
 
-  // add contraints (37e)
+  // add contraints (36e)
   for(int i = 0; i < n; ++i)
     for(int j = 0; j < i; ++j)
       model->addConstr(x[i][j] <= x[j][j]);
 
-  // add constraint (37f)
+  // add constraint (36f)
   model->addConstr(lu[0] <= p_bar);
-  // add constraint (37g)
+  // add constraint (36g)
   model->addConstr(lu[1] >= p_bar);
 
   model->update();
   model->write("debug_lu1.lp");
+
+  return x;
+}
+
+// add UL2 instance
+GRBVar** build_UL_2(GRBModel* model, graph* g, const vector<int>& population, int k)
+{
+  int n = g->nr_nodes;
+
+  GRBVar** x = new GRBVar*[n];
+  for(int i = 0; i < n; ++i)
+    x[i] = model->addVars(k, GRB_BINARY);
+
+  GRBVar* lu = model->addVars(2, GRB_CONTINUOUS); // L and U as variables
+  model->update();
+
+  model->setObjective(lu[1] - lu[0], GRB_MINIMIZE);
+
+  // add constraints (37b)
+  for(int i = 0; i < n; ++i)
+  {
+    GRBLinExpr constr = 0;
+    for(int j = 0; j < k; ++j)
+        constr += x[i][j];
+    model->addConstr(constr == 1);
+  }
+
+  // add constraint (37c)
+  for(int j = 0; j < k; ++j)
+  {
+    GRBLinExpr constr = 0;
+    for(int i = 0; i < n; ++i)
+      constr += population[i]*x[i][j];
+    // add for j
+    model->addConstr(constr - lu[1] <= 0); // U
+    model->addConstr(constr - lu[0] >= 0); // L
+  }
+
+  // explicitly fix for zero
+  for(int i = 0; i < n; ++i)
+    for(int j = 0; j < k; ++j)
+      if(i < j)
+        model->addConstr(x[i][j] == 0);
+
+  model->set(GRB_IntParam_Symmetry, 2);
+
+  model->update();
+  model->write("debug_lu2.lp");
 
   return x;
 }
