5321a1025f1b42be754849b6d376f19c720b9651
Eugene Lykhovyd <lykhovyd@gmail.com>
2018-12-07 19:40:00 -0600

    add mcf1

--- hess/flow.cpp
+++ hess/flow.cpp
@@ -5,7 +5,6 @@
 void build_scf(GRBModel* model, GRBVar** x, graph* g)
 {
   // create n^2 variables for arcs, presolve will eliminate unused
-  // TODO sophisticated indexing?
   int n = g->nr_nodes;
 
   GRBVar** y = new GRBVar*[n];
@@ -67,7 +66,52 @@ void build_scf(GRBModel* model, GRBVar** x, graph* g)
 
 void build_mcf1(GRBModel* model, GRBVar** x, graph* g)
 {
-  throw "Unimplemented!";
+  int n = g->nr_nodes;
+  // add additional flow variable (many) f[v][i][j]
+  GRBVar***f = new GRBVar**[n]; // commodity type, v
+  for(int v = 0; v < n; ++v)
+  {
+    f[v] = new GRBVar*[n]; // from node, i
+    for(int i = 0; i < n; ++i)
+      f[v][i] = model->addVars(n, GRB_BINARY); // to node, j
+  }
+
+  // add constraint (16b)
+  for(int i = 0; i < n; ++i)
+  {
+    for(int j = 0; j < n; ++j)
+    {
+      if(i == j)
+        continue;
+      GRBLinExpr expr = 0;
+      for(int nb_j : g->nb(j))
+      {
+        expr += f[i][j][nb_j]; // in d_+ : edge (j -- nb_j)
+        expr -= f[i][nb_j][j]; // in d_- : edge (nb_j -- j)
+      }
+      model->addConstr(expr - x[i][j] == 0);
+    }
+  }
+
+  // add constraint (16c)
+  for(int i = 0; i < n; ++i)
+  {
+    GRBLinExpr expr = 0;
+    for(int nb_i : g->nb(i))
+      expr += f[i][i][nb_i]; // in d_+ : edge (i -- nb_i)
+    model->addConstr(expr == 0);
+  }
+
+  // add constraint (16d)
+  // FIXME: i < j only or nah?
+  for(int i = 0; i < n; ++i)
+    for(int j : g->nb(i))
+      for(int v = 0; v < n; ++v)
+      {
+        if(v == i || v == j)
+          continue;
+        model->addConstr(f[v][i][j] - f[j][i][j] <= 0);
+      }
 }
 
 void build_mcf2(GRBModel* model, GRBVar** x, graph* g)
