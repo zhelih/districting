4cf8f57fc8c03ab7caceee6d19cbf8827d7e74f3
hamidrezavalidi <35265912+hamidrezavalidi@users.noreply.github.com>
2018-12-12 19:57:16 -0600

    Update cut.cpp

--- hess/cut.cpp
+++ hess/cut.cpp
@@ -2,13 +2,28 @@
 #include "graph.h"
 #include "gurobi_c++.h"
 
-//    model.getEnv().set(GRB_IntParam_LazyConstraints, 1);
 void build_cut1(GRBModel* model, GRBVar** x, graph* g)
 {
-  throw "Unimplemented!";
+	// create variables for arcs
+	int n = g->nr_nodes;
+
+	GRBVar** y = new GRBVar*[n];
+	for (int i = 0; i < n; ++i)
+		y[i] = model->addVars(n, GRB_BINARY);
+
+	// add constraint (23b)
+	for (int i = 0; i < n; ++i)
+	{
+		GRBLinExpr expr = 0;
+		for (int j : g->nb(i))
+		{
+			expr += y[j][i];
+		}
+		model->addConstr(expr + x[i][i] == 1);
+	}
 }
 
 void build_cut2(GRBModel* model, GRBVar** x, graph* g)
 {
-  throw "Unimplemented!";
+	throw "Unimplemented!";
 }
