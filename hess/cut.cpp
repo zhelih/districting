b4a05a82ef690b78b884e32af501edea124b72aa
hamidrezavalidi <35265912+hamidrezavalidi@users.noreply.github.com>
2018-12-15 11:38:34 -0600

    Update cut.cpp

--- hess/cut.cpp
+++ hess/cut.cpp
@@ -4,12 +4,19 @@
 
 void build_cut1(GRBModel* model, GRBVar** x, graph* g)
 {
-	// create variables for arcs
+	// create n^2 variables, and set UB=0 for non-arcs
 	int n = g->nr_nodes;
-
+	
 	GRBVar** y = new GRBVar*[n];
 	for (int i = 0; i < n; ++i)
-		y[i] = model->addVars(n, GRB_BINARY);
+	{
+		GRBVar *y_temp = new GRBVar[n];
+		for (int j = 0; j < n; ++j)
+		{
+			y_temp[j] = model->addVar(0.0, 0.0, 0.0, GRB_BINARY);
+		}
+		y[i] = y_temp;
+	}
 
 	// add constraint (23b)
 	for (int i = 0; i < n; ++i)
@@ -17,6 +24,7 @@ void build_cut1(GRBModel* model, GRBVar** x, graph* g)
 		GRBLinExpr expr = 0;
 		for (int j : g->nb(i))
 		{
+			y[j][i].set(GRB_DoubleAttr_UB, 1.0);
 			expr += y[j][i];
 		}
 		model->addConstr(expr + x[i][i] == 1);
