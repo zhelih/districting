1677b65ac2b2888b53e7360090f0ca5fb3953f46
hamidrezavalidi <35265912+hamidrezavalidi@users.noreply.github.com>
2019-01-15 23:06:24 -0600

    Create LazyConstraints.cpp

---
+++ hess/LazyConstraints.cpp
@@ -0,0 +1,124 @@
+#include "LazyConstraints.h"
+#include "gurobi_c++.h"
+#include "graph.h"
+#include <algorithm>
+#include <cmath>
+#include <ctime>
+#include <queue>
+#include <set>
+#include <map>
+#include <omp.h>
+#include <memory>
+#include <sstream>
+#include <vector>
+using namespace std;
+
+long LazyConstraints::numCallbacks = 0;
+double LazyConstraints::TotalCallbackTime = 0;
+long LazyConstraints::numLazyCuts = 0;
+
+
+GRBVar **varsx;
+GRBVar **varsy;
+graph *g1;
+
+
+LazyConstraints::LazyConstraints(GRBVar **xvars, GRBVar **yvars, graph *g)
+{
+	varsx = xvars;
+	varsy = yvars;
+	g1=g->duplicate();
+};
+
+void LazyConstraints::callback() {
+	try {
+		//cerr << "Hi" << endl;
+		if (where == GRB_CB_MIPSOL) // Found an integer ``solution'' that satisfies all vertex cut constraints so far.
+		{
+
+			numCallbacks++;
+			time_t start = clock();
+
+			double **X = new double*[g1->nr_nodes];
+			for (int i = 0; i < g1->nr_nodes; ++i)
+			{
+				X[i] = new double[g1->nr_nodes];
+				for (int j = 0; j < g1->nr_nodes; ++j)
+					X[i][j] = (getSolution(varsx[i][j]) > 0.5) ? 1 : 0;
+			}
+
+			double **Y = new double*[g1->nr_nodes];
+			for (int i = 0; i < g1->nr_nodes; ++i)
+			{
+				Y[i] = new double[g1->nr_nodes];
+				for (int j = 0; j < g1->nr_nodes; ++j)
+					Y[i][j] = (getSolution(varsy[i][j]) > 0.5) ? 1 : 0;
+			}
+			for (int i = 0; i < g1->nr_nodes; ++i)
+			{
+				int root;
+				vector<bool> S(g1->nr_nodes, false);
+				for (int q = 0; q < g1->nr_nodes; q++)
+				{
+					if (q == i)
+						S[q] = true;
+					if (X[i][q] == 1)
+						root = q;
+				}
+				//do a BFS
+				vector<int> children, parents;
+				children.push_back(i);
+				for (; !children.empty(); )
+				{ //do BFS
+					parents = children;
+					children.clear();
+					for (int u = 0; u<parents.size(); ++u)
+					{ //for each parent, examine the children
+						int p = parents[u];
+						//int j = -1;
+						for (int v : g1->nb(p))
+						{
+							if (!S[v] && Y[v][p])
+							{ //can only use vertices in S
+								S[v] = true;
+								children.push_back(v);
+							}
+						}
+					}
+				}
+				if (S[root]) continue;
+				GRBLinExpr expr1 = 0;
+				GRBLinExpr expr2 = 0;
+				for (int q = 0; q < g1->nr_nodes; q++)
+				{
+					if (S[q])
+						expr1 += varsx[i][q];
+				}
+				for (int q = 0; q < g1->nr_nodes; q++)
+				{
+					if (!S[q]) continue;
+					for (int u : g1->nb(q))
+					{
+						//int u = g1.adj[q][j];
+						if (!S[u])
+							expr2 += varsy[u][q];
+					}
+				}
+				//Adding Lazycuts
+				addLazy(expr1 + expr2 >= 1);
+				numLazyCuts++;
+			}
+
+			TotalCallbackTime += (double)(clock() - start) / CLOCKS_PER_SEC;
+			delete[] X;
+			delete[] Y;
+		}
+	}
+	catch (GRBException e) {
+		cout << "Error number: " << e.getErrorCode() << endl;
+		cout << e.getMessage() << endl;
+	}
+	catch (...) {
+		cout << "Error during callback" << endl;
+	}
+}
