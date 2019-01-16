41ff1cefd57fe15f50fcf4bb669e56f1a18a03e9
hamidrezavalidi <35265912+hamidrezavalidi@users.noreply.github.com>
2019-01-15 23:07:29 -0600

    Create LazyConstraints.h

---
+++ hess/LazyConstraints.h
@@ -0,0 +1,30 @@
+#ifndef LAZYCONSTRAINTS_H
+#define LAZYCONSTRAINTS_H
+
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <vector>
+#include <map>
+#include "Models.h"
+#include "gurobi_c++.h"
+#include <ctime>
+#include <memory>
+#include <sstream>
+#include "graph.h"
+
+
+extern double copy_time;
+using namespace std;
+
+class LazyConstraints : public GRBCallback //Lazy constraints for solving CUT1 in political redistricting
+{
+public:
+	LazyConstraints(GRBVar **xvars, GRBVar **yvars, graph *g);
+	void callback();
+	static long numCallbacks;
+	static double TotalCallbackTime;
+	static long numLazyCuts;
+};
+
+#endif
