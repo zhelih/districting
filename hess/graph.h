9c38b7fc43306cec291c82115311c1f5c46876e6
Eugene Lykhovyd <lykhovyd@gmail.com>
2019-03-02 17:03:05 -0600

    fix compile to ISO

--- hess/graph.h
+++ hess/graph.h
@@ -20,10 +20,10 @@ public:
     void remove_edge(uint i, uint j);
     std::vector<int>& nb(uint i) { return nb_[i]; }
     bool is_connected(); // TODO const;
-    vector<int> graph::preprocess(vector<int>& new_population, vector<bool>& deleted, int L, int U);
-    vector<int> graph::findUnderPopulatedLeaves(vector<int> new_population, vector<bool> deleted, int L);
+    vector<int> preprocess(vector<int>& new_population, vector<bool>& deleted, int L, int U);
+    vector<int> findUnderPopulatedLeaves(vector<int> new_population, vector<bool> deleted, int L);
     void clean(vector<int>& new_population, vector<bool>& deleted, int L, int U, int& numOfEdgeDel, int& numOfNodeMerge);
-    void graph::edgeClean(vector<int>& population, int U);
+    void edgeClean(vector<int>& population, int U);
     //bool graph::deleteEdge(int i, int j);
     // works as far as no pointers are members
     graph* duplicate() const { return new graph(*this); }
