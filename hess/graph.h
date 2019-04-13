a0c54080a1da95a9b3345065245debbe391f2be9
hamidrezavalidi <35265912+hamidrezavalidi@users.noreply.github.com>
2019-04-12 22:15:38 -0500

    Update graph.h

--- hess/graph.h
+++ hess/graph.h
@@ -4,6 +4,8 @@
 typedef unsigned int uint;
 
 #include <vector>
+#include <stack>
+
 using namespace std;
 
 class graph
@@ -20,18 +22,25 @@ public:
     void remove_edge(uint i, uint j);
     std::vector<int>& nb(uint i) { return nb_[i]; }
     bool is_connected(); // TODO const;
-    vector<int> preprocess(vector<int>& new_population, vector<bool>& deleted, int L, int U);
+    
     vector<int> findUnderPopulatedLeaves(vector<int> new_population, vector<bool> deleted, int L);
     void clean(vector<int>& new_population, vector<bool>& deleted, int L, int U, int& numOfEdgeDel, int& numOfNodeMerge);
-    void edgeClean(vector<int>& population, int U);
+    void edgeClean(const vector<int>& population, int U);
     //bool graph::deleteEdge(int i, int j);
     // works as far as no pointers are members
     graph* duplicate() const { return new graph(*this); }
     int get_k() const { return k; }
     void set_k(int k_) { k = k_; }
-
-    void connect(const std::vector<std::vector<int>>& dist); // make the graph connected
+    void connect(const std::vector<std::vector<int>>& dist); // make the graph connected    
 };
+
+vector<vector<int>> FindBiconnectedComponents(graph* g, vector<int> &AV, vector<bool> &deletedNodes);
+
+vector<vector<int>> preprocess(graph* g, vector<int>& new_population, vector<int>& stem, int L, int U, const vector<int>& population);
+
+void Bico_Sub(int v, int u, int &i, graph* g, vector<int> &number, vector<int> &lowopt, stack<int> &le, stack<int> &re, vector< vector<int>> &BC, vector<bool> &deletedNodes);
+
+vector<int> findUnderPopulatedCompLeaves(vector<vector<int>>& preClusters, vector<int>& new_population, const vector<int>& population, vector<int>& AV, vector<bool>& activePreClusters, int L);
 
 graph* from_dimacs(const char* fname); // don't forget to delete
 
