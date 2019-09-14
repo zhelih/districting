62d01a540124b0cc3a5c9a2c995de90f09344676
Hamidreza Validi <hamidreza.validi2@gmail.com>
2019-09-14 13:35:32 -0500

    applied Austin's comments on the function

--- hess/findHotDuals.cpp
+++ hess/findHotDuals.cpp
@@ -11,21 +11,27 @@
 
 using namespace std;
 
-hess_params findHotDual(GRBModel* model, graph* g, const vector<vector<double> >& w, const vector<int>& population, int L, int U, int k, cvv& F0, cvv& F1)
+void find_LP_dual_solution(GRBModel* model, graph* g, run_params rp, const vector<vector<double> >& w, const vector<int>& population, int L, int U, int k)
 {
 
     // create GUROBI Hess model
     int n = g->nr_nodes;
     hess_params p;
     p.n = n;
-    p.F0 = F0; p.F1 = F1; // copy
+    //p.F0 = F0; p.F1 = F1; // copy
 
     // hash variables
     int cur = 0;
     for (int i = 0; i < n; ++i)
+    {
         for (int j = 0; j < n; ++j)
-            if (!F0[i][j] && !F1[i][j])
-                p.h[n*i + j] = cur++; //FIXME implicit reuse of the map (i,j) -> n*i+j
+        {
+            //cerr << i << ", " << j << endl;
+            //if (!F0[i][j] && !F1[i][j])
+            p.h[n*i + j] = cur++; //FIXME implicit reuse of the map (i,j) -> n*i+j
+        }            
+    }
+        
 
     printf("Build hess : created %lu variables\n", p.h.size());
     int nr_var = static_cast<int>(p.h.size());
@@ -34,11 +40,19 @@ hess_params findHotDual(GRBModel* model, graph* g, const vector<vector<double> >
     p.x = model->addVars(nr_var, GRB_CONTINUOUS);
     model->update();
 
+    //cerr << "Hi there!" << endl;
+
     // Set objective: minimize sum d^2_ij*x_ij
     GRBLinExpr expr = 0;
     for (int i = 0; i < n; ++i)
+    {
         for (int j = 0; j < n; ++j)
-            expr += w[i][j] * X(i, j);
+        {
+            //cerr << i << ", " << j << endl;
+            expr += w[i][j] * X_V(i, j);
+        }        
+    }
+        
 
     model->setObjective(expr, GRB_MINIMIZE);
 
@@ -47,7 +61,7 @@ hess_params findHotDual(GRBModel* model, graph* g, const vector<vector<double> >
     {
         GRBLinExpr constr = 0;
         for (int j = 0; j < n; ++j)
-            constr += X(i, j);
+            constr += X_V(i, j);
         model->addConstr(constr == 1);
     }
 
@@ -58,8 +72,8 @@ hess_params findHotDual(GRBModel* model, graph* g, const vector<vector<double> >
     {
         GRBLinExpr constr = 0;
         for (int i = 0; i < n; ++i)
-            constr += population[i] * X(i, j);
-        model->addConstr(constr - L * X(j, j) >= 0);
+            constr += population[i] * X_V(i, j);
+        model->addConstr(constr - L * X_V(j, j) >= 0);
     }
 
     // add constraint (d) for population upper bound
@@ -67,24 +81,54 @@ hess_params findHotDual(GRBModel* model, graph* g, const vector<vector<double> >
     {
         GRBLinExpr constr = 0;
         for (int i = 0; i < n; ++i)
-            constr += population[i] * X(i, j);
-        model->addConstr(constr - U * X(j, j) <= 0);
+            constr += population[i] * X_V(i, j);
+        model->addConstr(constr - U * X_V(j, j) <= 0);
     }
 
     // add constraint (c)
     expr = 0;
     for (int j = 0; j < n; ++j)
-        expr += X(j, j);
+        expr += X_V(j, j);
     model->addConstr(expr == k);
 
     // add contraints (e)
     for (int i = 0; i < n; ++i)
         for (int j = 0; j < n; ++j)
-            if (i != j && !F0[i][j])
-                model->addConstr(X(i, j) <= X(j, j));
+            //if (i != j && !F0[i][j])
+                model->addConstr(X_V(i, j) <= X_V(j, j));
 
     model->update();
 
-    return p;
+    model->set(GRB_IntParam_Method, 2);
+    model->set(GRB_IntParam_OutputFlag, 0);
+    model->set(GRB_IntParam_Crossover, 0);
+
+    model->optimize();
+
+    if (model->get(GRB_IntAttr_Status) == 3) // infeasible
+        cerr << "Infeasible!" << endl;
+
+    //define constraints
+    GRBConstr *c = 0;
+    c = model->getConstrs();
+
+    int i;
+
+    //create a file
+    FILE* f;
+    string dualHot_fn = string(rp.state) + "_" + "hotDual" + ".hot";
+    f = fopen(dualHot_fn.c_str(), "w");
+
+    //print dual vars in file
+
+    for (i = 0; i < n; ++i)
+        fprintf(f, "%.6lf\n", c[i].get(GRB_DoubleAttr_Pi));
+
+    for (; i < 2 * n; ++i)
+        fprintf(f, "%.6lf\n", L * c[i].get(GRB_DoubleAttr_Pi));
+
+    for (; i < 3 * n; ++i)
+        fprintf(f, "%.6lf\n", U * c[i].get(GRB_DoubleAttr_Pi));
+
 }
 
